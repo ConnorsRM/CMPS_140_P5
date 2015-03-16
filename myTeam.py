11323ff0d029d30f126e9f0a1a6af705d908a947
ConnorsRM <ryan@Ryans-MacBook-Air.local>
2015-03-15 17:58:18 -0700

    DefenseAgent Parital implementation
    Nearly fucking there… my brain is mush.
    I apologize for not having this in much better shape

--- myTeam.py
+++ myTeam.py
@@ -63,7 +63,7 @@ class DummyAgent(CaptureAgent):
   """
   #add more states with comma after Assault
   class States:
-    Assault, Nommer = range(2)
+    Assault, Nommer, Defend = range(3)
   
   
   #store state
@@ -71,6 +71,20 @@ class DummyAgent(CaptureAgent):
   assaultTgt    = None
   isTop         = None
   nommerTarget  = None
+  
+  #Defense properties
+  defIsOnRed      = None
+  defMyTeamInd    = None
+  defEnemyTeamInd = None
+  #Defense Observations:
+  defenseBeliefs  = None
+  legalPositions  = None
+  DEFABDEPTH      = 2
+  defCurrentDepth = 0
+  #Defense Patrol FIeld
+  currentDirection = None
+  initialAssault = True
+  #Store which Ghosts are still Respawning
   
 
   def registerInitialState(self, gameState):
@@ -99,6 +113,23 @@ class DummyAgent(CaptureAgent):
       self.minSpanTreeWeight(foodList)
     
     self.beginAssault(gameState)
+    
+    #set redteamflag
+    if(gameState.isOnRedTeam(self.index)):
+      self.defIsOnRed = True
+    else :
+      self.defIsOnRed = False
+    
+    if (self.defIsOnRed):
+      self.defMyTeamInd    = gameState.getRedTeamIndices()
+      self.defEnemyTeamInd = gameState.getBlueTeamIndices()
+    else :
+      self.defMyTeamInd    = gameState.getBlueTeamIndices()
+      self.defEnemyTeamInd = gameState.getRedTeamIndices()
+    
+    self.legalPositions = self.getLegalPositions(gameState)
+    
+    self.beginDefense(gameState)
     ''' 
     Your initialization code goes here, if you need any.
     '''
@@ -110,7 +141,12 @@ class DummyAgent(CaptureAgent):
       return successor.generateSuccessor(self.index, action)
     else:
       return successor
-  
+
+  """
+   ASSAULT AGENT:
+   uses a greedy search agent with a mazedistance based simple
+   heuristic to move accross border as soon as possible.
+  """
   #return the best available action in assault mode
   #using a simple greedy search to be augmented later
   def beginAssault(self, gameState):
@@ -159,8 +195,7 @@ class DummyAgent(CaptureAgent):
       
     
     self.assaultTgt = (newTarget[0], newTarget[1])
-    
-    
+  
   def AssaultAction(self, gameState):
     self.beginAssault(gameState)
     actions = gameState.getLegalActions(self.index)
@@ -208,6 +243,344 @@ class DummyAgent(CaptureAgent):
       tgtDistFeat = 1.0
     
     return (yWeight * yProxFeat) + (distWeight * tgtDistFeat)
+
+  """
+     DEFENSE AGENT
+  """
+  def getLegalPositions(self, gameState):
+    thisInitialPos = gameState.getInitialAgentPosition(self.index)
+    enemyInitialPos = gameState.getInitialAgentPosition(3 - self.index)
+    maxX  = max(thisInitialPos[0], enemyInitialPos[0])
+    maxY  = max(thisInitialPos[1], enemyInitialPos[1])
+    
+    #we can derive a list os positions from this,
+    #just remove those where hasWall is true
+    legalPos = []
+    
+    for xPos in range(1, maxX):
+      for yPos in range(1, maxY):
+        if not gameState.hasWall(xPos, yPos):
+          legalPos += [(xPos, yPos)]
+    
+    return legalPos
+    
+  def believedPos(self, gameState):
+    enemyTeamPositions = []
+    
+    for index in self.defEnemyTeamInd:
+      enemyTeamPositions += [self.defenseBeliefs[index].argMax()]
+    
+    enemyAObs = gameState.getAgentPosition(self.defEnemyTeamInd[0])
+    enemyBObs = gameState.getAgentPosition(self.defEnemyTeamInd[1])
+    
+    if enemyAObs:
+      enemyTeamPositions[0] = enemyAObs
+    
+    if enemyBObs:
+      enemyTeamPositions[1] = enemyBObs
+    
+    
+    return enemyTeamPositions
+  
+  def beginDefense(self, gameState):
+    #initialize beliefs uniformly
+    self.defenseBeliefs = util.Counter()
+    self.defenseBeliefs[self.defEnemyTeamInd[0]] = util.Counter()
+    self.defenseBeliefs[self.defEnemyTeamInd[1]] = util.Counter()
+    
+    
+    for pos in self.legalPositions:
+      self.defenseBeliefs[self.defEnemyTeamInd[0]][pos] = 0.0001
+      self.defenseBeliefs[self.defEnemyTeamInd[1]][pos] = 0.0001
+    
+    self.defenseBeliefs[self.defEnemyTeamInd[0]][gameState.getInitialAgentPosition(self.defEnemyTeamInd[0])] = 1.0
+    self.defenseBeliefs[self.defEnemyTeamInd[1]][gameState.getInitialAgentPosition(self.defEnemyTeamInd[1])] = 1.0
+    
+    self.defenseBeliefs[self.defEnemyTeamInd[0]].normalize()
+    self.defenseBeliefs[self.defEnemyTeamInd[1]].normalize()
+    
+    self.defCurrentDepth = 0
+    
+    
+    return
+  
+  
+  def DefenseAction(self, gameState):
+    self.DefenseObserve(gameState)
+    enemyPositions = self.believedPos(gameState)
+    
+    thisInitialPos = gameState.getInitialAgentPosition(self.index)
+    enemyInitialPos = gameState.getInitialAgentPosition(3 - self.index)
+    border = int(max(thisInitialPos[0],enemyInitialPos[0]))/2
+    myState   = gameState.getAgentState(self.index)
+    myPos     = myState.getPosition()
+    evalFunction = self.DefensePatrolHeuristic
+    
+    #if stay at least 2 grids outside of spawn
+    if(abs(myPos[0] - border) > 5):
+      self.beginAssault(gameState)
+      return self.AssaultAction(gameState)
+    else :
+      self.initialAssault = False
+    
+    for position in enemyPositions:
+      #if closer to my spawn then theirs
+      if(abs(position[0] - thisInitialPos[0]) < abs(position[0] - enemyInitialPos[0])):
+        evalFunction = self.DefenseCatchHeuristic
+    
+    #now use selected heuristic to govern action
+    actions = gameState.getLegalActions(self.index)
+    if (Directions.STOP in actions):
+      actions.remove(Directions.STOP)
+    
+    bestVal    = None
+    bestAction = None
+    for action in actions:
+      if(bestAction == None):
+        bestAction = action
+        bestVal = evalFunction(gameState, action)
+      else :
+        thisVal = evalFunction(gameState, action)
+        if (thisVal > bestVal):
+          bestVal = thisVal
+          bestAction = action
+    
+    
+    return bestAction
+  
+  
+  def DefensePatrolHeuristic(self, gameState, action):
+    #get enemy Positions
+    #if not in our territory, patrol
+    #else capture
+    newState = gameState.generateSuccessor(self.index, action)
+    
+    #ensure we're not clumping
+    clumpMotive = 0.0
+    
+    aPos = newState.getAgentPosition(self.defMyTeamInd[0])
+    bPos = newState.getAgentPosition(self.defMyTeamInd[1])
+    
+    clumpDist = self.getMazeDistance(aPos, bPos)
+    
+    if(clumpDist < 5):
+      clumpMotive -= 0.5
+    
+    
+    #stay away from spawn
+    myState   = gameState.getAgentState(self.index)
+    myPos     = myState.getPosition()
+    initial   = gameState.getInitialAgentPosition(self.index)
+    
+    spawnMotive = util.manhattanDistance(myPos, initial)
+    
+    #one step look ahead
+
+    return clumpMotive + spawnMotive
+
+  def DefenseCatchHeuristic(self, gameState, action):
+    thisInitialPos = gameState.getInitialAgentPosition(self.index)
+    enemyInitialPos = gameState.getInitialAgentPosition(3 - self.index)
+    enemyPositions = self.believedPos(gameState)
+    myState   = gameState.getAgentState(self.index)
+    myPos     = myState.getPosition()
+    
+    closestEnemy = enemyPositions[0]
+    closestDist  = self.getMazeDistance(myPos, enemyPositions[0])
+    
+    for position in enemyPositions:
+      if (abs(position[0] - thisInitialPos[0]) < abs(position[0] - thisInitialPos[0])):
+        thisDist =   util.manhattanDistance(myPos, position)
+        if thisDist < closestDist:    
+          closestEnemy = position
+          closestDist = thisDist
+    #find closestEnemy
+    
+    print "DIST"
+    print closestDist
+    if(closestDist == 0): return float("inf")
+    return -1.0*(closestDist)
+  
+  def DefenseEval(self, gameState):
+    #we want to ensure superiority
+    
+    #   measures:
+    #  1.) less food -> less superior
+    #  2.) pacman is close to initial spawn than I am -> less superior
+    
+    foodWeight = 0.9
+    posWeight  = 0.01
+    
+    foodMotive = 0
+    
+    #spin on turings algorithm
+    #my food * value of food divided by their food * val of food
+    #all foods the same, so lets just drop that
+    myFood    = (len(self.getFoodYouAreDefending(gameState).asList()))
+    enemyFood = (len(self.getFood(gameState).asList()))
+    
+    try:
+      foodMotive = myFood/enemyFood
+    except ZeroDivisionError:
+      foodMotive = myFood
+    
+    
+    enemyPositions = self.believedPos(gameState)
+    myState   = gameState.getAgentState(self.index)
+    myPos     = myState.getPosition()
+    posMotive = 0
+    myHome    = gameState.getInitialAgentPosition(self.index)
+    
+    for position in enemyPositions:
+      if (position == None): continue
+      if(abs(myHome[0] - position[0]) < abs(myHome[0] - myPos[0])):
+        posMotive -= 1
+    
+    
+    return (foodMotive * foodWeight) + (posMotive * posWeight)
+  
+  """
+  def DefenseMaxVal(self, gameState, agent, alpha, beta):
+    self.defCurrentDepth += 1
+    if self.DefenseDepthCheck(gameState) :
+      return [self.DefenseEval(gameState), Directions.STOP]
+    
+    v = [float("-inf"), Directions.STOP]
+    
+    actions = actions = gameState.getLegalActions(agent)
+
+    if (Directions.STOP in actions):
+      actions.remove(Directions.STOP)
+    
+    newAgent = agent + 1
+    if(agent == gameState.getNumAgents() -1):
+      newAgent = 0
+    
+    for thisAction in actions:
+      newState = gameState.generateSuccessor(agent, thisAction)
+      thisUtility = self.abVal(newState, newAgent, alpha, beta)[0]
+      if thisUtility > v[0]:
+        v = [thisUtility, thisAction]
+      if v[0] >= beta:
+        return v
+      alpha = max(alpha, v[0])
+    return v
+  
+  def DefenseEnemyLegalActions(self, gameState, agent):
+    legalActions = [Directions.STOP]
+    #get believed position
+    enemyPos = self.defenseBeliefs[agent].argMax()
+    print "-=-=-=-"
+    print enemyPos
+    print "********"
+    #this is gross, but there is not other way of accessing
+    #I can find.
+    
+    #lateral limits
+    thisInitialPos = gameState.getInitialAgentPosition(self.index)
+    enemyInitialPos = gameState.getInitialAgentPosition(3 - self.index)
+    maxX = max(thisInitialPos[0], enemyInitialPos[0])
+    maxY = max(thisInitialPos[1], enemyInitialPos[1])
+    subOneX = enemyPos[0] - 1
+    subOneY = enemyPos[1] - 1
+    addOneX = enemyPos[0] + 1
+    addOneY = enemyPos[1] + 1
+    
+    
+    if (subOneX >= 1 and not gameState.hasWall(subOneX, enemyPos[1])):
+      legalActions += [Directions.WEST]
+    if (addOneX <= maxX and not gameState.hasWall(addOneX, enemyPos[1])):
+      legalActions += [Directions.EAST]
+    if (subOneY >= 1 and not gameState.hasWall(enemyPos[0], subOneY)):
+      legalActions += [Directions.SOUTH]
+    if (addOneY <= maxY and not gameState.hasWall(enemyPos[0], addOneY)):
+      legalActions += [Directions.NORTH]
+    
+    
+    return legalActions
+  
+  def abVal(self, gameState, agent, alpha, beta):
+    if (agent in self.defEnemyTeamInd):
+      return self.DefenseMinVal(gameState, agent, alpha, beta)
+    else:
+      return self.DefenseMaxVal(gameState, agent, alpha, beta)
+  
+  
+  def DefenseMinVal(self, gameState, agent, alpha, beta):
+    if self.DefenseDepthCheck(gameState) :
+      return [self.DefenseEval(gameState), Directions.STOP]
+    
+    v = [float("inf"), Directions.STOP]
+    
+    actions = self.DefenseEnemyLegalActions(gameState, agent)
+    
+    for index in self.defEnemyTeamInd:
+      print self.DefenseEnemyLegalActions(gameState, index)
+      print "=-=-=-=-="
+    print "================="
+    if (Directions.STOP in actions):
+      actions.remove(Directions.STOP)
+    
+    newAgent = agent + 1
+    if(agent == gameState.getNumAgents() - 1):
+      newAgent = 0
+    
+    for thisAction in actions:
+      print agent
+      print thisAction
+      try :
+        newState = gameState.generateSuccessor(agent, thisAction)
+      except :
+        newState = gameState
+      
+      thisUtility = self.abVal(newState, newAgent, alpha, beta)[0]
+      
+      if thisUtility < v[0]:
+        v = [thisUtility, thisAction]
+      
+      if v[0] <= alpha:
+        return v
+      
+      beta = min(beta, v[0])
+    
+    return v
+  
+  def DefenseDepthCheck(self, gameState):
+    if ((self.defCurrentDepth / 2) >= self.DEFABDEPTH
+      or (gameState.isOver())):                            return True
+    
+    return False
+  
+  """
+  def DefenseObserve(self, gameState):
+    
+    noisyDistance = self.getCurrentObservation().getAgentDistances()
+    
+    
+    #for each noisyDistance, use getDistanceProb
+    #to obtain P(noisyDist | True)
+    
+    #start with first enemy
+    enemyAAgentIndex = self.defEnemyTeamInd[0]
+    enemyBAgentIndex = self.defEnemyTeamInd[1]
+    
+    newBelief = util.Counter()
+    newBelief[self.defEnemyTeamInd[0]] = util.Counter()
+    newBelief[self.defEnemyTeamInd[1]] = util.Counter()
+    myState   = gameState.getAgentState(self.index)
+    myPos     = myState.getPosition()
+    
+    for position in self.legalPositions:
+      knownDistance = util.manhattanDistance(position, myPos)
+      newProb = self.defenseBeliefs[enemyAAgentIndex][position] * gameState.getDistanceProb(noisyDistance[enemyAAgentIndex], knownDistance)
+      newBelief[enemyAAgentIndex][position] = newProb
+      newProb = self.defenseBeliefs[enemyBAgentIndex][position] * gameState.getDistanceProb(noisyDistance[enemyBAgentIndex], knownDistance)
+      newBelief[enemyBAgentIndex][position] = newProb
+    
+    newBelief[enemyAAgentIndex].normalize()
+    newBelief[enemyBAgentIndex].normalize()
+    self.defenseBeliefs = newBelief
+
   
   def NommerAction(self, gameState):
     actions = gameState.getLegalActions(self.index)
@@ -346,7 +719,9 @@ class DummyAgent(CaptureAgent):
     enemyInitialPos = gameState.getInitialAgentPosition(3 - self.index)
     border = int(max(thisInitialPos[0],enemyInitialPos[0]))/2
     myPos = gameState.getAgentState(self.index).getPosition()
-      
+    
+    self.state = self.States.Defend
+    
     if (self.state == self.States.Assault):
       if (((myPos[0] > thisInitialPos[0]) and (myPos[0] > border)) or
             ((myPos[0] < thisInitialPos[0]) and (myPos[0] < border))):
@@ -373,6 +748,8 @@ class DummyAgent(CaptureAgent):
       return self.AssaultAction(observation)
     elif (self.state == self.States.Nommer):
       return self.NommerAction(observation)
+    elif (self.state == self.States.Defend):
+      return self.DefenseAction(observation)
     
     return random.choice(actions)
     
